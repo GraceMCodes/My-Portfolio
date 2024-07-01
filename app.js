08e7fdfdb9ebe34217cd9166b63b75f4f33064bd
Grace M codes <gracemcodes@gmail.com>
2024-07-01 17:12:23 +0300

    I added the time, in JS , customized the color .

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ var typed = new Typed(".typing",{
  typeSpeed:100,
  BackSpeed:60,
 loop:true})
+
+var now = new Date();
+var datetime = now.toLocaleString();
+
+
+document.getElementById("datetime").innerHTML = datetime;
